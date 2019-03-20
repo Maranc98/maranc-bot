8f505d54fb63416ac6f50e47e54fff6fd7631754
Maranc98 <maranc98@gmail.com>
2019-03-20 19:53:34 +0100

    Added update functionality

--- start_bot.sh
+++ start_bot.sh
@@ -6,6 +6,13 @@ result=65;
 
 while [ $result -eq 65 ]
 do
-${python_path} ${python_script}
-result=$?
+${python_path} ${python_script};
+result=$?;
+
+if [ $result -eq 66 ];
+then
+  echo "Updating bot";
+  git pull;
+  result=65;
+fi
 done
