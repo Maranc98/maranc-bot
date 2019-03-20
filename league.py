ae70f06b64a450bc219a5e620940eb9a44e74cd2
Maranc98 <maranc98@gmail.com>
2019-03-20 23:40:17 +0100

    Added a way to set the lol api token remotely

--- league.py
+++ league.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import requests
+import re
 
 from tokens import LOL_TOKEN
 from utils import ms_to_text
@@ -36,10 +37,54 @@ async def league(update, bot):
 async def settoken(update, bot):
     new_token = update['text'].split(' ')
 
+
     if(len(new_token) < 2):
         return "You have to write a token for this to work! .. Or were you trying to take my precious key? 😠"
 
     new_token = new_token[1]
+    words = re.split('-',new_token)
+
+    if(len(new_token) != 42 or
+       len(words) != 6 or
+       len(words[0]) != 5 or
+       len(re.findall('[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z]',words[0])) != 1 or
+       len(words[1]) != 8 or
+       len(re.findall('[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]',words[1])) != 1 or
+       len(words[2]) != 4 or
+       len(words[3]) != 4 or
+       len(words[4]) != 4 or
+       len(re.findall('[a-z0-9][a-z0-9][a-z0-9][a-z0-9]',words[2])) != 1 or
+       len(re.findall('[a-z0-9][a-z0-9][a-z0-9][a-z0-9]',words[3])) != 1 or
+       len(re.findall('[a-z0-9][a-z0-9][a-z0-9][a-z0-9]',words[4])) != 1 or
+       len(words[5]) != 12 or
+       len(re.findall('[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]',words[5])) != 1
+       ):
+       monkas = open('badppl.csv','a')
+       date = update['date']
+       text = update['text']
+
+       #sistemare, e' un po' bruttino
+       if 'from' in update:
+           sender = update['from']
+           if 'username' in sender:
+               username = sender['username']
+           else:
+               username = 'None'
+
+           if 'last_name' in sender:
+               last_name = sender['last_name']
+           else:
+               last_name = 'None'
+       else:
+           sender = {'first_name' : 'None', 'is_bot' : 'None', 'id' : 'None'}
+           last_name = 'None'
+           username = 'None'
+
+
+       monkas.write(sender['first_name'] + ',' + last_name + ',' + username + ',' + str(sender['is_bot']) + ',' + str(sender['id']) + ',' + str(date) + ',' + text + '\n')
+       monkas.close()
+       return "You have just set LOL_TOKEN to " + new_token + ". Restart the bot to start using the new token!"
+
     tokens_file = open('tokens.py', 'r')
     text = tokens_file.read()
     tokens_file.close()
